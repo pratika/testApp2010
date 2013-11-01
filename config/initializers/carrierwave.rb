71dd59feff8896ca5b2cee670217dd542688eb75
Pratik Agarwal <pagarwal@mba2014.hbs.edu>
2013-11-01 18:08:53 -0400

    renaming AWS keys in initializers

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,10 +5,10 @@ CarrierWave.configure do |config|
     config.storage = :fog
     config.fog_credentials = {
       :provider => 'AWS',
-      :aws_access_key_id => ENV['AWS_ACCESS_KEY'],
-      :aws_secret_access_key => ENV['AWS_SECRET_KEY']
+      :aws_access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :aws_secret_access_key => ENV['AWS_SECRET_KEY_ID']
     }
  
-    config.fog_directory = ENV['AWS_BUCKET']
+    config.fog_directory = ENV['S3_BUCKET_NAME']
   end
 end
